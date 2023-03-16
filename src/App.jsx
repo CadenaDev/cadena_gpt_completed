4fbf9b50febad1f8f28f061c45e4161ab8111c45
Saeed Jabbar <saeedryanjabbar@gmail.com>
2023-03-16 01:27:30 -0400

    added padding for mobile friendly

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center px-4 sm:p-0">
       <img src={cadenaLogo} alt="Cadena Logo" className="h-12 mb-10 mt-40" />
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
         <div className="flex items-center border-b-2 border-indigo-600 py-2">
